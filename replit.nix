9323aa5b718d75bfeb43b0b92bc5769295c3aabf
MatthewSabia1 <MatthewSabia1@users.noreply.github.com>
2025-02-06 21:32:02 -0500

    fix: Minimal Replit configuration to resolve recovery mode

--- replit.nix
+++ replit.nix
@@ -1,35 +1,8 @@
+# replit.nix
 { pkgs }: {
   deps = [
-    # Core dependencies
     pkgs.python39
-    pkgs.nodejs-16_x
-    pkgs.cmake
-    pkgs.gcc
-    pkgs.pkg-config
-    pkgs.bzip2
-
-    # Python packages
     pkgs.python39Packages.pip
-    pkgs.python39Packages.setuptools
-    pkgs.python39Packages.wheel
-    pkgs.python39Packages.numpy
-    pkgs.python39Packages.flask
-    pkgs.python39Packages.flask-cors
-    pkgs.python39Packages.opencv4
+    pkgs.nodejs-16_x
   ];
-
-  env = {
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.zlib
-      pkgs.bzip2
-      pkgs.openssl
-      pkgs.libxml2
-      pkgs.libxslt
-      pkgs.libjpeg
-      pkgs.openblas
-      pkgs.boost
-      pkgs.opencv4
-    ];
-    PYTHONPATH = "${pkgs.python39Packages.opencv4}/lib/python3.9/site-packages";
-  };
 } 
